7ab49ab32c34fc1b54b6ec82d6f4dd0733ffc5d0
ponmak <peeranut.4498@gmail.com>
2025-04-03 17:34:57 +0700

    enum and structs done

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -1,3 +1,5 @@
+use std::string;
+
 #[derive(Debug)]
 struct Point {
     x: u64,
@@ -7,6 +9,11 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Resize{width : i32, height : i32},
+    Move(Point),
+    Echo(String),
+    ChangeColor(i32,i32,i32),
+    Quit
 }
 
 impl Message {
